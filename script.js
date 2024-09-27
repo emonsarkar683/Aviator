2d5d1538894acac88dfd50399853ea5e723119f8
MD Emon Sarkar <98849715+emonsarkar683@users.noreply.github.com>
2024-09-27 20:41:04 +0600

    Update script.js
    Signed-off-by: MD Emon Sarkar <98849715+emonsarkar683@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('loadGame').addEventListener('click', function() {
-    const url = 'https://cdnet-launch.apac.spribegaming.com/aviator?currency=BDT&lang=en&user=0vt160817457&operator=tcgf2c3ebb969896-55c8-8d64-4498-3b38430a';
+    const url ='https://launch.spribegaming.com/aviator?currency=BDT&lang=en&user=17s171619041&operator=tcgbdt&token=f2-99cb5ba5a54b-af19-37b4-c235-94c09788';
 
     // Create an iframe to load the game
     const gameContent = document.getElementById('gameContent');
